7f6ebde4fdfb6a923921bbefd5f02b4f756c2247
mayu2312 <lostbutterfly2312@gmail.com>
2026-04-30 23:41:17 +0700

    Change entirely

--- otomasi.py
+++ otomasi.py
@@ -6,10 +6,9 @@ from datetime import datetime
 
 subprocess.Popen('notepad.exe') # open notepad
 time.sleep(2) # wait for notepad to open
-pyautogui.write('Your Mom is Gay', interval=0.1) # type the text with a delay between each character
+pyautogui.write('Your Mom is Gay', interval=0.05) # type the text with a delay between each character
 pyautogui.press('enter')
-
-img = pyautogui.screenshot() # take a screenshot
-img.save(os.path.join(os.getcwd(), f'screenshot_{datetime.now().strftime("%Y%m%d_%H%M%S")}.png')) # save the screenshot with a timestamp in the filename
-
-print('Screenshot taken and saved successfully.') # print a success message
+pyautogui.write('Just Messing Around, CIAO', interval=0.05)
+pyautogui.press('enter')
+pyautogui.write(f'Checking Time RN {datetime.now().strftime("%H:%M")}', interval=0.05)
+pyautogui.press('enter')
