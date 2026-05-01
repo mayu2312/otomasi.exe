0a85455615a2a7763462fe8dbece9fa775a1475d
mayu2312 <lostbutterfly2312@gmail.com>
2026-05-01 15:35:40 +0700

    Update error github cs2

--- random_otomasi.py
+++ random_otomasi.py
@@ -4,12 +4,11 @@ import subprocess
 import time
 from datetime import datetime
 
-subprocess.Popen(r'C:\Program Files (x86)\Steam\steamapps\common\Counter-Strike 2\game\bin\win64\cs2.exe')
+subprocess.Popen(r'C:\Program Files (x86)\Steam\steamapps\common\Counter-Strike Global Offensive\game\bin\win64\cs2.exe')
 time.sleep(10) # Wait for the game to launch
 
 subprocess.Popen('notepad.exe')
 time.sleep(2) # Wait for Notepad to open
 
 pyautogui.write("Your CS is now open, let's play together init", interval=0.05)
-
-
+pyautogui.press('enter')
