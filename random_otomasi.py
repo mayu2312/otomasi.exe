d8c6726312c415615a53c836971e159383ec3fc8
mayu2312 <lostbutterfly2312@gmail.com>
2026-05-01 15:08:01 +0700

    Creating CS2 Opener

---
+++ random_otomasi.py
@@ -0,0 +1,15 @@
+import os
+import pyautogui # type: ignore # python -m pip install pyautogui
+import subprocess
+import time
+from datetime import datetime
+
+subprocess.Popen(r'C:\Program Files (x86)\Steam\steamapps\common\Counter-Strike 2\game\bin\win64\cs2.exe')
+time.sleep(10) # Wait for the game to launch
+
+subprocess.Popen('notepad.exe')
+time.sleep(2) # Wait for Notepad to open
+
+pyautogui.write("Your CS is now open, let's play together init", interval=0.05)
+
+
